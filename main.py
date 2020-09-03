1b773d9c35bdf4214143dbcd553d0155a8260f5b
Khalil Stroman <7208392@philasd.org>
2020-09-03 22:59:07 +0000

    fixing

--- main.py
+++ main.py
@@ -5,12 +5,12 @@
 # Collaborator: Matthew Orehek muo53@psu.edu
 degree = input("Enter temperature: ")
 degree = float(degree)
-unit = input("Enter unit in F/f or C/c:")
+unit = input("Enter unit in F/f or C/c: ")
 if unit == "C" or unit == "c": 
   fah = (degree * 1.8) +32 
   print(f"{degree}° in Celsius is equivalent to {fah}° in Fahrenheit.") 
 elif unit == "F" or unit == "f" : 
   cel = (degree-32)/1.8
-  print(f"{degree}° in Fahrenheit is equivalent to {cel}° Celsius")
+  print(f"{degree}° in Fahrenheit is equivalent to {cel}° Celsius.")
 else : 
   print("Invalid unit(bad).")
